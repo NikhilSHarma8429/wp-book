a7a116f07563a440dccd21685f3167d5c2404947
Nikhil Sharma <nikhil.sharma@hbwsl.com>
2025-07-26 13:23:49 +0530

    Create a custom non-hierarchical taxonomy Book Tag

--- wp-book.php
+++ wp-book.php
@@ -76,3 +76,35 @@
         register_taxonomy( 'book_category', array( 'book' ), $args );
     }
     add_action( 'init', 'wp_book_register_book_category_taxonomy' );
+
+    function wp_book_register_book_tag_taxonomy() {
+        $labels = array(
+            'name'                       => __( 'Book Tags', 'wp-book' ),
+            'singular_name'              => __( 'Book Tag', 'wp-book' ),
+            'search_items'               => __( 'Search Book Tags', 'wp-book' ),
+            'popular_items'              => __( 'Popular Book Tags', 'wp-book' ),
+            'all_items'                  => __( 'All Book Tags', 'wp-book' ),
+            'edit_item'                  => __( 'Edit Book Tag', 'wp-book' ),
+            'update_item'                => __( 'Update Book Tag', 'wp-book' ),
+            'add_new_item'               => __( 'Add New Book Tag', 'wp-book' ),
+            'new_item_name'              => __( 'New Book Tag Name', 'wp-book' ),
+            'separate_items_with_commas' => __( 'Separate tags with commas', 'wp-book' ),
+            'add_or_remove_items'        => __( 'Add or remove book tags', 'wp-book' ),
+            'choose_from_most_used'      => __( 'Choose from the most used book tags', 'wp-book' ),
+            'menu_name'                  => __( 'Book Tags', 'wp-book' ),
+        );
+
+        $args = array(
+            'hierarchical'          => false, // Like post tags
+            'labels'                => $labels,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'update_count_callback' => '_update_post_term_count',
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => 'book-tag' ),
+            'show_in_rest'          => true
+        );
+
+        register_taxonomy( 'book_tag', 'book', $args );
+    }
+    add_action( 'init', 'wp_book_register_book_tag_taxonomy' );
