3c2de3a65bb0db56124f199939637b7d3d559649
Nikhil Sharma <nikhil.sharma@hbwsl.com>
2025-07-26 13:03:19 +0530

    Initializing the Plugin

---
+++ wp-book.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+        * Plugin Name: Csv Data Uploader
+        * Author: Nikhil
+        * Version: 1.0.0
+        * Description: This is a Csv Data Uploader Plugin
+        * Author URI: https://example.com
+        * Plugin URI: https://example.com
+    */
+
+    if(!defined('ABSPATH')) exit;
+
+    
